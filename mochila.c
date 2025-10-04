a51612ad8df921615b901b8bd5c0e32e7d74d6a9
KauaTrombim <kauabenjamint@gmail.com>
2025-10-04 00:16:43 -0300

    Adicionando a função de criação e impressão de itens aleatórios

--- mochila.c
+++ mochila.c
@@ -1,37 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h> 
+#include <time.h>
+
 
 typedef struct{
-    double peso;
+    int id;
+    double peso; // para usarmos MOD (%) precisamos de inteiros, isso nos leva a usar o peso em g e não em Kg
     int valor;
+    double razao;
 } item;
 
 typedef struct{
-    double maxW;
-    //item *itens_guardados;
+    int maxW;
+    item *itens_guardados;
 
 } Mochila;
 
-int main(){
-    int n, w; //n é o número de itens, w o peso máximo suportado pela mochila
-    printf("Insira o número de itens: ");
-    scanf("%d", &n);
+void criarItens(item *lista, int n, int w){
+    for(int i = 0; i < n; i++){
+        item aux;
 
-    printf("Insira o peso máximo: ");
-    scanf("%d", &w);
-    
-    item *itens;
-    itens = (item *) calloc(n, sizeof(item));
+        //Geração de valores aleatórios que não sejam todos perfeitamente finos ou maiores que W
+        int min = w/n;
+        double delta =  ((double) rand()/RAND_MAX) * (2 *min);
+        printf("delta %d: %.2lf\n", i, delta); //debug
+        
+        aux.peso = min + delta;
+        aux.valor = aux.peso * delta;
+        aux.razao = aux.valor/aux.peso;
+        aux.id = i;
+        lista[i] = aux;
+    }
+}
 
-    //Será que é realmente necessário guardar os ítens em um vetor simulando a mochila?
-    //item *guardados;
-    //guardados = (item *) calloc(w, sizeof(item));
-
-    Mochila *mochila;
-    mochila->maxW = w;
-
-    return 0;
+void imprimirItens(item *lista, int n){
+    int pesoTotal = 0;
+    for(int i = 0; i < n; i++){
+        pesoTotal += lista[i].peso;
+        printf("%d Peso: %.2lf\n", i, lista[i].peso);
+        printf("%d Valor: %d\n", i, lista[i].valor);
+    }
+    printf("Peso total dos itens: %d\n", pesoTotal);
 }
 
 void forcaBruta(item *lista, Mochila *mochila){
     
 }
+
+
+int main(){
+    int n;
+    int w;
+    printf("Insira o número de itens: ");
+    scanf("%d", &n);
+
+    printf("Insira o peso máximo da mochila: ");
+    scanf("%d", &w);
+    
+    item itens[n];
+    srand(time(NULL)); //inicia o gerador de sementes a partir do tempo atual
+    criarItens(itens, n, w);
+    imprimirItens(itens, n);
+
+    Mochila *mochila = (Mochila *) malloc(sizeof(mochila));
+    mochila->maxW = w;
+    
+
+    return 0;
+}
+
